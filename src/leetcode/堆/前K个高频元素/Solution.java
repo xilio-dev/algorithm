bd204de9ab62d9b58570f1eb7a2f9292eaeb7d71
jack <xilio1024@gmail.com>
2025-08-11 10:19:49 +0800

    feat: 环形链表

---
+++ src/leetcode/堆/前K个高频元素/Solution.java
@@ -0,0 +1,40 @@
+package leetcode.堆.前K个高频元素;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] res = new int[k];
+        Map<Integer, Integer> frequency = new HashMap<>();
+        //统计频率 {数字:频率}
+        for (int num : nums) {
+            frequency.merge(num, 1, Integer::sum);
+        }
+        //小顶堆
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+            minHeap.offer(new int[]{entry.getKey(), entry.getValue()});
+            //限制堆的大小，只保留前k个元素 ，节省空间
+            if (minHeap.size() > k) {
+                //弹出对顶频率小的元素
+                minHeap.poll();
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            res[i] = minHeap.poll()[0];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 1, 2, 2, 3};
+        Solution solution = new Solution();
+        int[] res = solution.topKFrequent(nums, 2);
+        for (int i : res) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/leetcode/链表/环形链表II/Solution2.java
@@ -0,0 +1,53 @@
+package leetcode.链表.环形链表II;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution2 {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != head) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(0);
+        ListNode node3 = new ListNode(-4);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node1;
+        Solution2 solution = new Solution2();
+        ListNode node = solution.detectCycle(head);
+        System.out.println(node.val);
+    }
+}
