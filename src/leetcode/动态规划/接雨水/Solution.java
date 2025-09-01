fc431fa95b851765240e9ef0704dc4d4cb50c211
jack <xilio1024@gmail.com>
2025-09-01 12:21:35 +0800

    feat: 接雨水

---
+++ src/leetcode/动态规划/接雨水/Solution.java
@@ -0,0 +1,35 @@
+package leetcode.动态规划.接雨水;
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n <= 2) {
+            return 0; // 无法形成凹槽接雨水
+        }
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int totalWater = 0;
+        for (int i = 0; i < n; i++) {
+            totalWater += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return totalWater;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        Solution solution = new Solution();
+        int result = solution.trap(height);
+        System.out.println(result);
+    }
+}
